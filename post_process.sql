8c32a8d3b7e583824958076d1ef9a5b445452272
Jon Bramley <brammers@gmail.com>
2021-05-20 17:50:51 +0100

    feat: load brownfield-land into geom table for mapping

--- post_process.sql
+++ post_process.sql
@@ -20,6 +20,7 @@ CREATE TABLE geography_geom (
     type
 );
 SELECT AddGeometryColumn('geography_geom', 'geom', 4326, 'MULTIPOLYGON', 2);
+SELECT AddGeometryColumn('geography_geom', 'geom_point', 4326, 'POINT', 2);
 
 INSERT INTO geography_geom (rowid, geojson_simple, geojson_full, type, geom)
 SELECT
@@ -32,6 +33,18 @@ FROM
     geography AS g
 JOIN slug AS s ON g.slug_id = s.id
 WHERE json_valid(AsGeoJSON(GeomFromText(g.geometry))) = 1;
+
+INSERT INTO geography_geom (rowid, geojson_simple, geojson_full, type, geom_point)
+SELECT
+    g.rowid AS rowid,
+    json_object('type', 'Feature', 'id', g.rowid, 'properties', json_object('name', g.name, 'type', g.type, 'slug', s.slug, 'rowid', g.rowid, 'entry-date', entry_date, 'start-date', start_date, 'end-date', end_date), 'geometry', json(AsGeoJSON(Simplify(GeomFromText(g.point, 4326), 0.0005)))) AS geojson_simple,
+    json_object('type', 'Feature', 'id', g.rowid, 'properties', json_object('name', g.name, 'type', g.type, 'slug', s.slug, 'rowid', g.rowid, 'entry-date', entry_date, 'start-date', start_date, 'end-date', end_date), 'geometry', json(AsGeoJSON(GeomFromText(g.point, 4326)))) AS geojson_full,
+    g.type AS type,
+    GeomFromText(g.point, 4326) AS geom_point
+FROM
+    geography AS g
+JOIN slug AS s ON g.slug_id = s.id
+WHERE json_valid(AsGeoJSON(GeomFromText(g.point))) = 1;
 
 SELECT CreateSpatialIndex("geography_geom", "geom");
 SELECT count(*) AS geography_count FROM geography_geom;
