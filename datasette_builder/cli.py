b1153539080ba0c8c6cf59dc107e8f24ac967554
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2021-09-01 10:44:44 +0100

    lint.

--- datasette_builder/cli.py
+++ datasette_builder/cli.py
@@ -27,9 +27,7 @@ def collect(config_path):
 
 
 @click.command()
-@click.argument(
-    "metadata_path", type=click.Path(exists=True), default="./"
-)
+@click.argument("metadata_path", type=click.Path(exists=True), default="./")
 def build_view_queries(metadata_path):
     with open(Path(metadata_path) / "metadata.json", "r") as json_file:
         metadata = defaultdict(None, json.load(json_file))
