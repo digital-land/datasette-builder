376fa5a80a32132e70b60b70145a0cd19b8b6a94
Paul Downey <psd@whatfettle.com>
2021-12-20 18:48:00 +0000

    Download entity database from collection-dataset s3

--- startup.sh
+++ startup.sh
@@ -4,27 +4,15 @@ mkdir -p specification
 curl -qfsL 'https://raw.githubusercontent.com/digital-land/specification/main/specification/dataset.csv' > specification/dataset.csv 
 
 collection_s3="https://collection-dataset.s3.eu-west-2.amazonaws.com/"
-
 dl_s3="https://digital-land-collection.s3.eu-west-2.amazonaws.com/"
 
-declare -a datasets=("entity" 
-                "digital-land"
-                )
 set -x
 date
 set +x
 
-for dataset in "${datasets[@]}"
-do
-    url=$dl_s3$dataset.sqlite3
-    path=$dataset.sqlite3
-
-    if [ ! -f $path ] ; then
-        set -x
-        curl -qsfL -o $path "$url"
-        set +x
-    fi
-done
+set -x
+curl -qsfL -o digital-land.sqlite3 ${dl_s3}digital-land.sqlite3
+curl -qsfL -o entity.sqlite3 ${collection_s3}entity.sqlite3
 
 IFS=,
 csvcut -c dataset,collection specification/dataset.csv |
@@ -52,4 +40,3 @@ date
 set +x
 
 gunicorn app:app -t 60 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:5000
-
