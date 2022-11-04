8b0350b044d0b138e9b1e7f3f1c6611c2e6f62ed
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-04 12:16:53 +0000

    Remove break after a few downloads

--- download-files.sh
+++ download-files.sh
@@ -6,9 +6,6 @@ BUCKET=$1
 
 FILES=$(aws s3api list-objects --bucket "$BUCKET" --output json --query "Contents[?ends_with(Key, 'sqlite3')]" | jq -rc '.[].Key')
 for FILE in $FILES; do
-  if [[ $FILE == "article-4-direction-collection/dataset/article-4-direction-area.sqlite3" ]]; then
-    break
-  fi
   echo "Downloading $FILE"
   aws s3api get-object --bucket "$BUCKET" --key "$FILE" "./files/$(basename "$FILE")" > /dev/null
   aws s3api get-object --bucket "$BUCKET" --key "$FILE.json" "./files/$(basename "$FILE").json" > /dev/null || echo "no inspect files"
