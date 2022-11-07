fdd44d063bb368b2783ed38e9570fc1a3a361be7
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-07 08:44:29 +0000

    Re-evaluate available sqlite files on restart

--- startup.sh
+++ startup.sh
@@ -3,13 +3,12 @@
 DATASETTE_SERVE_ARGS="-h 0.0.0.0 -p $PORT --setting sql_time_limit_ms 5000 --nolock "
 DATASETTE_PID=0
 
-for FILE in /mnt/datasets/*.sqlite3; do
-  DATASETTE_SERVE_ARGS+="--immutable=$FILE "
-done
-
-DATASETTE_SERVE_ARGS+=" --inspect-file=/mnt/datasets/inspect-data-all.json --template-dir=/app/templates/"
-
 start_datasette() {
+  for FILE in /mnt/datasets/*.sqlite3; do
+    DATASETTE_SERVE_ARGS+="--immutable=$FILE "
+  done
+
+  DATASETTE_SERVE_ARGS+=" --inspect-file=/mnt/datasets/inspect-data-all.json --template-dir=/app/templates/"
   echo "Starting datasette service with args $DATASETTE_SERVE_ARGS"
   if [[ "$DATASETTE_PID" -ne "0" ]]; then kill $DATASETTE_PID; fi
   datasette serve ${DATASETTE_SERVE_ARGS} & DATASETTE_PID=$! || exit 1
